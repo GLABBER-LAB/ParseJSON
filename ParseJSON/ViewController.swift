a73230abb4fa73d865614e95f6204a5bb0e1e98a
GLABBER <chernigovmaster@gmail.com>
2021-12-18 23:44:39 +0200

    Add Json1

--- ParseJSON/ViewController.swift
+++ ParseJSON/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let urlString = "https://icodeschool.ru/json1.php"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error{
+                print(error)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            let  jsonString = String(data: data, encoding: .utf8)
+            print(jsonString)
+            
+        }.resume()
     }
 
 
 }
+
 
